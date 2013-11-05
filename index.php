759aa3e660c73ac09a050e25fa7a4087aba1c0cf
Butch Page <butchpage1@gmail.com>
2013-11-04 16:13:18 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>The Complete Guide to the Settings API | Sandbox Theme</title>
+	</head>
+	<body>
+		<div id="header">
+			<h1>Sandbox Header</h1>
+		</div> <!-- end header -->
+
+		<div id="content">
+			<p>This is the content.</p>
+		</div><!-- end content -->
+
+		<div id="footer">
+			<p>&copy; <?php echo date('Y'); ?> All Rights Reserved.</p>
+		</div><!-- end footer -->
+	</body>
+</html>
